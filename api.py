dc724828ac3743a3063aa65ff83ce7ef673e3870
Cole Yee <n.lindsey.yee@gmail.com>
2024-01-28 16:20:56 -0800

    Get nested filesystems working
    - Leverage flask routing capabilities allowing `GET /bar/baz/` to return
      `baz1.txt`
    - TODO: make env variable able to handle trailing slash or no trailing
      slash when setting the "context" folder
    - TODO: Better error handling
    - TODO: simple tests!
    - TODO: move what makes sense out of the "one big" function that
      currently exists
    
    Time Spent: 30 minutes

--- api.py
+++ api.py
@@ -2,19 +2,25 @@ import os
 import subprocess
 from flask import Flask
 
+# currently trailing slash needed in WORK_DIR variable when set in env
 work_dir = os.environ["WORK_DIR"]
+print("WORK_DIR: ", work_dir)
 
 app = Flask(__name__)
 
-@app.route("/")
-def ls(path=None):
-    result = subprocess.run(['ls', work_dir], capture_output=True, text=True)
-    return "Result: " + result.stdout
-
-@app.route("/<path>")
-def get(path=None):
-    path = work_dir + path + "/"
+@app.route("/<path:params>")
+def ls(params):
+    path = work_dir + params
     print("path: ", path)
-    result = subprocess.run(['ls', path], capture_output=True, text=True)
+
+    file_contents = subprocess.run(['cat', path], capture_output=True, text=True)
+    print("file_contents err: ", file_contents.stderr)
+
+    if file_contents.stderr:
+        path = path + "/"
+        result = subprocess.run(['ls', path], capture_output=True, text=True)
+    else:
+        result = file_contents
+
     print("err: ", result.stderr)
     return "Result: " + result.stdout
