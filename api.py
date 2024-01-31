98195039fe8c4499eb3b5b91cacc28d59225841a
Cole Yee <n.lindsey.yee@gmail.com>
2024-01-30 17:51:50 -0800

    Learn more Flask things and DRY up code
    Time Spent: 5 minutes

--- api.py
+++ api.py
@@ -6,28 +6,8 @@ def create_app(path_to_files):
     app = Flask(__name__)
 
     @app.route("/")
-    def get_root_ls():
-        path = set_work_dir(path_to_files)
-
-        file_contents = subprocess.run(['cat', path], capture_output=True, text=True)
-        print("file_contents err: ", file_contents.stderr)
-
-        if file_contents.stderr:
-            path = path + "/"
-            result = parse_ls_result(path)
-        else:
-            # TODO: return a proper JSON response with file name, file type, owner, and size
-            result = file_contents.stdout
-
-        response_json = {
-                "success": True,
-                "message": "File system accessed sucessfully",
-                "data": { "files": result }
-                }
-        return response_json
-
     @app.route("/<path:params>")
-    def ls(params):
+    def ls(params=''):
         path = set_work_dir() + params
         print("path: ", path)
 
